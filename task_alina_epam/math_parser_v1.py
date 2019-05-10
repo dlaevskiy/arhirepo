9611b3d93d13d03dc1464bc3887f970ce5a6281c
dlaevskiy <dlaevskiy@kyriba.com>
2019-05-10 19:08:29 +0300

    fix

--- task_alina_epam/math_parser_v1.py
+++ task_alina_epam/math_parser_v1.py
@@ -10,6 +10,7 @@ OPERATORS = {'+': (1, lambda x, y: x + y),
              '-': (1, lambda x, y: x - y),
              '*': (2, lambda x, y: x * y),
              '/': (2, lambda x, y: x / y),
+             'sin': (0, lambda x: math.sin(x))
              # '//': (2, lambda x, y: x // y),
              # '%': (2, lambda x, y: x % y),
              # '^': (2, lambda x, y: x ^ y),
@@ -38,11 +39,11 @@ def parse(formula_string):
         # 1. finding function
         if elem in string.ascii_lowercase or is_function is True:
             is_function = True
-            if elem != ')':  # try to find the end of the function
+            if elem != '(':  # try to find the end of the name of the math function
                 function += elem
             else:
-                function += elem  # to add ')' at the end of the function
                 yield function
+                yield '('
                 function = ''
                 is_function = False
         else:
@@ -60,41 +61,16 @@ def parse(formula_string):
                     operator = ''
                 else:
                     yield elem
-            # else:
-            #     raise ValueError('Incorrect symbol in formula: {}!'.format(elem)) # TODO to think how process incorrect symbols
     if number:  # если в конце строки есть число, выдаём его
         yield float(number)
     if operator:
         raise ValueError('Incorrect formula: operator at the end of the formula!')
 
 
-# L = []
-# for elem in parse('1+sin(0.3)'):
-#     L.append(elem)
-#
-# print L
-
-
-# calculation of the functions
-
-def calc_functions_in_list(parsed_list):
-    new_L = []
-    for elem in parsed_list:
-        if isinstance(elem, str) and '(' in elem and ')' in elem:
-            parse_function = re.split('\(|\)', elem)
-            function_name = parse_function[0]
-            function_argument = parse_function[1]
-            try:
-                value_of_function = getattr(math, function_name)(float(function_argument))
-                new_L.append(value_of_function)
-            except AttributeError:
-                raise ValueError('Error in formula_string! Function: {} is not exist in library math!'.format(function_name))
-            except ValueError:
-                raise ValueError('Error in formula_string! Incorrect argument in function {}(): {}!'.format(function_name,
-                                                                                                            function_argument))
-        else:
-            new_L.append(elem)
-    return new_L
+parsed_list = []
+for element in parse('1+5*4'):
+    parsed_list.append(element)
+print parsed_list
 
 
 # [7.0, '*', 5.5, '+', 3.0]
@@ -106,11 +82,8 @@ def shunting_yard(parsed_formula):
         # до открывающей скобки или опустошения стека.
         # здесь мы пользуемся тем, что все операторы право-ассоциативны
         if token in OPERATORS:
-            while stack:
-                prior_current_oper = OPERATORS[token][0]
-                prior_last_stack_oper = OPERATORS[stack[-1]][0]
-                if stack[-1] != "(" and prior_current_oper <= prior_last_stack_oper:
-                    yield stack.pop()
+            while stack and stack[-1] != "(" and OPERATORS[token][0] <= OPERATORS[stack[-1]][0]:
+                yield stack.pop()
             stack.append(token)
         elif token == ")":
             # если элемент - закрывающая скобка, выдаём все элементы из стека, до открывающей скобки,
@@ -130,11 +103,10 @@ def shunting_yard(parsed_formula):
         yield stack.pop()
 
 
-# L = []
-# for elem in shunting_yard([7.0, '*', 5.5, '+', 3.0]):
-#     L.append(elem)
-#
-# print L
+polish_list = []
+for element in shunting_yard(parsed_list):
+    polish_list.append(element)
+print polish_list
 
 
 def calc(polish):
@@ -147,21 +119,6 @@ def calc(polish):
             stack.append(token)
     return stack[0]  # результат вычисления - единственный элемент в стеке
 
-
-parsed_list = []
-for element in parse('1+2*3'):
-    parsed_list.append(element)
-print parsed_list
-
-calculated_list = []
-for element in calc_functions_in_list(parsed_list):
-    calculated_list.append(element)
-print calculated_list
-
-polish_list = []
-for element in shunting_yard(calculated_list):
-    polish_list.append(element)
-print polish_list
 
 result = calc(polish_list)
 
