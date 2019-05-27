d93a01816b985bdbebc5ac451c137d5c27442f2f
Ali Lavska <46864916+alinonax@users.noreply.github.com>
2019-05-27 21:25:45 +0200

    Update unittests_parse_function.py

--- task_alina_epam/unittests_parse_function.py
+++ task_alina_epam/unittests_parse_function.py
@@ -382,25 +382,230 @@ class TestParsing(unittest.TestCase):
         for el in parse('.+1'):
             list_.append(el)
         self.assertEqual(list_, ['.', '+', 1.0, ])
-    
+
+    #logarithms
     def test61(self):
-        self.assertRaises(ValueError, lambda: parse('1+&6.0'))
+        list_ = []
+        for el in parse('log(8,2)'):
+            list_.append(el)
+        self.assertEqual(list_, ['log', '(', 8.0, ',', 2.0, ')'])
 
     def test62(self):
-        self.assertRaises(ValueError, lambda: parse(['_+6']))
+        list_ = []
+        for el in parse('log(2.7)'):
+            list_.append(el)
+        self.assertEqual(list_, ['log', '(', 2.7, ')'])
 
     def test63(self):
-        self.assertRaises(ValueError, lambda: parse(['1>=@']))
+        list_ = []
+        for el in parse('(log(8,2)-1)-2'):
+            list_.append(el)
+        self.assertEqual(list_, ['(', 'log', '(', 8.0, ',', 2.0, ')', '-', 1.0, ')', '-', 2.0])
 
     def test64(self):
-        self.assertRaises(ValueError, lambda: parse(['-2+(#+1)']))
+        list_ = []
+        for el in parse('-log(8,2)'):
+            list_.append(el)
+        self.assertEqual(list_, ['-', 'log', '(', 8.0, ',', 2.0, ')'])
 
     def test65(self):
-        self.assertRaises(ValueError, lambda: parse(['1..4+6']))
+        list_ = []
+        for el in parse('log(8,2)-1'):
+            list_.append(el)
+        self.assertEqual(list_, ['log', '(', 8.0, ',', 2.0, ')', '-', 1.0])
 
     def test66(self):
-        self.assertRaises(ValueError, lambda: parse(['1.4+6..']))
+        list_ = []
+        for el in parse('log(8,2)*log(16,2)'):
+            list_.append(el)
+        self.assertEqual(list_, ['log', '(', 8.0, ',', 2.0, ')', '*', 'log', '(', 16.0, ',', 2.0, ')'])
 
     def test67(self):
-        self.assertRaises(ValueError, lambda: parse(['1..4+6..1']))
-        
+        list_ = []
+        for el in parse('sin(log(8,2)*log(16,2))'):
+            list_.append(el)
+        self.assertEqual(list_, ['sin', '(', 'log', '(', 8.0, ',', 2.0, ')', '*', 'log', '(', 16.0, ',', 2.0, ')', ')'])
+
+    def test68(self):
+        list_ = []
+        for el in parse('log(8+20-1,2+1)'):
+            list_.append(el)
+        self.assertEqual(list_, ['log', '(', 8.0, '+', 20.0, '-', 1.0, ',', 2.0, '+', 1.0, ')'])
+
+    def test69(self):
+        list_ = []
+        for el in parse('log10(100)'):
+            list_.append(el)
+        self.assertEqual(list_, ['log10', '(', 100.0, ')'])
+
+    def test70(self):
+        list_ = []
+        for el in parse('log(100,10)'):
+            list_.append(el)
+        self.assertEqual(list_, ['log', '(', 100.0, ',', 10.0, ')'])
+
+    def test71(self):
+        list_ = []
+        for el in parse('(log10(100)-1)-2'):
+            list_.append(el)
+        self.assertEqual(list_, ['(', 'log10', '(', 100.0, ')', '-', 1.0, ')', '-', 2.0])
+
+    def test72(self):
+        list_ = []
+        for el in parse('-log10(100)'):
+            list_.append(el)
+        self.assertEqual(list_, ['-', 'log10', '(', 100.0, ')'])
+
+    def test73(self):
+        list_ = []
+        for el in parse('log10(100)-1'):
+            list_.append(el)
+        self.assertEqual(list_, ['log10', '(', 100.0, ')', '-', 1.0])
+
+    def test74(self):
+        list_ = []
+        for el in parse('log10(100)*log10(1000)'):
+            list_.append(el)
+        self.assertEqual(list_, ['log10', '(', 100.0, ')', '*', 'log10', '(', 1000.0, ')'])
+
+    def test75(self):
+        list_ = []
+        for el in parse('sin(log10(100)*log10(1000))'):
+            list_.append(el)
+        self.assertEqual(list_, ['sin', '(', 'log10', '(', 100.0, ')', '*', 'log10', '(', 1000.0, ')', ')'])
+
+    def test76(self):
+        list_ = []
+        for el in parse('log10(800/2/4)'):
+            list_.append(el)
+        self.assertEqual(list_, ['log10', '(', 800.0, '/', 2.0, '/', 4.0, ')'])
+
+    # pow
+    def test77(self):
+        list_ = []
+        for el in parse('pow(2,4)'):
+            list_.append(el)
+        self.assertEqual(list_, ['pow', '(', 2.0, ',', 4.0, ')'])
+
+    def test78(self):
+        list_ = []
+        for el in parse('log(pow(10,2),10)'):
+            list_.append(el)
+        self.assertEqual(list_, ['log', '(', 'pow', '(', 10.0, ',', 2.0, ')', ',', 10.0, ')'])
+
+    def test79(self):
+        list_ = []
+        for el in parse('(pow(2,4)-1)-2'):
+            list_.append(el)
+        self.assertEqual(list_, ['(', 'pow', '(', 2.0, ',', 4.0, ')', '-', 1.0, ')', '-', 2.0])
+
+    def test80(self):
+        list_ = []
+        for el in parse('-pow(2,4)'):
+            list_.append(el)
+        self.assertEqual(list_, ['-', 'pow', '(', 2.0, ',', 4.0, ')'])
+
+    def test81(self):
+        list_ = []
+        for el in parse('pow(2,4)-1'):
+            list_.append(el)
+        self.assertEqual(list_, ['pow', '(', 2.0, ',', 4.0, ')', '-', 1.0])
+
+    def test82(self):
+        list_ = []
+        for el in parse('pow(2,4)*pow(2,3)'):
+            list_.append(el)
+        self.assertEqual(list_, ['pow', '(', 2.0, ',', 4.0, ')', '*', 'pow', '(', 2.0, ',', 3.0, ')'])
+
+    def test83(self):
+        list_ = []
+        for el in parse('sin(pow(2,4)*pow(2,3))'):
+            list_.append(el)
+        self.assertEqual(list_, ['sin', '(', 'pow', '(', 2.0, ',', 4.0, ')', '*', 'pow', '(', 2.0, ',', 3.0, ')', ')'])
+
+    def test84(self):
+        list_ = []
+        for el in parse('pow(2.0^(2.0^2.0*2.0^2.0),sin(log10(100)*log10(1000)))'):
+            list_.append(el)
+        self.assertEqual(list_, ['pow', '(', 2.0, '^', '(', 2.0, '^', 2.0, '*', 2.0, '^', 2.0, ')', ',', 'sin', '(',
+                                 'log10', '(', 100.0, ')', '*', 'log10', '(', 1000.0, ')', ')', ')'])
+
+    # Incorrect symbol in formula
+    def test85(self):
+        self.assertRaises(ValueError, lambda: parse('1+&6.0'))
+
+    def test86(self):
+        self.assertRaises(ValueError, lambda: parse('1+6.0&'))
+
+    def test87(self):
+        self.assertRaises(ValueError, lambda: parse(['_5+6']))
+
+    def test88(self):
+        self.assertRaises(ValueError, lambda: parse(['5_+6']))
+
+    def test89(self):
+        self.assertRaises(ValueError, lambda: parse(['1>=@']))
+
+    def test90(self):
+        self.assertRaises(ValueError, lambda: parse(['1@>=9']))
+
+    def test91(self):
+        self.assertRaises(ValueError, lambda: parse(['-2+(#+1)']))
+
+    def test92(self):
+        self.assertRaises(ValueError, lambda: parse(['abs(@)']))
+
+    def test93(self):
+        self.assertRaises(ValueError, lambda: parse(['round(@)']))
+
+    def test94(self):
+        self.assertRaises(ValueError, lambda: parse(['sin(5+@)']))
+
+    def test95(self):
+        self.assertRaises(ValueError, lambda: parse(['sin(@+5)']))
+
+    def test96(self):
+        self.assertRaises(ValueError, lambda: parse(['(5+@)/7']))
+
+    def test97(self):
+        self.assertRaises(ValueError, lambda: parse(['1+#+6']))
+
+    def test98(self):
+        self.assertRaises(ValueError, lambda: parse(['1+#8+6']))
+
+    # Number with more than one delimiter
+    def test99(self):
+        self.assertRaises(ValueError, lambda: parse(['1..5']))
+
+    def test100(self):
+        self.assertRaises(ValueError, lambda: parse(['-1..5']))
+
+    def test101(self):
+        self.assertRaises(ValueError, lambda: parse(['1..5+3..7']))
+
+    def test102(self):
+        self.assertRaises(ValueError, lambda: parse(['1..5+3']))
+
+    def test103(self):
+        self.assertRaises(ValueError, lambda: parse(['3+1..5']))
+
+    def test104(self):
+        self.assertRaises(ValueError, lambda: parse(['1+1..5-4']))
+
+    def test105(self):
+        self.assertRaises(ValueError, lambda: parse(['sin(1..5)']))
+
+    def test106(self):
+        self.assertRaises(ValueError, lambda: parse(['(1..5+3)/2']))
+
+    def test107(self):
+        self.assertRaises(ValueError, lambda: parse(['round(1..5)']))
+
+    def test108(self):
+        self.assertRaises(ValueError, lambda: parse(['abs(-1..5)']))
+
+    def test109(self):
+        self.assertRaises(ValueError, lambda: parse(['1>=1..5']))
+
+if __name__ == '__main__':
+    unittest.main()
