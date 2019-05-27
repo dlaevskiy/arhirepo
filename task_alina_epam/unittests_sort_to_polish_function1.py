fcfdf23a71f2cae22016aa6e96a2c082abed94f2
Ali Lavska <46864916+alinonax@users.noreply.github.com>
2019-05-27 21:32:00 +0200

    Update unittests_sort_to_polish_function1.py

--- task_alina_epam/unittests_sort_to_polish_function1.py
+++ task_alina_epam/unittests_sort_to_polish_function1.py
@@ -220,7 +220,7 @@ class TestSorting(unittest.TestCase):
                                   '+', 2.0, '^', 2.0, '+', 1.0, ',', 3.0, '^', 2.0, ')']):
             list_.append(el)
         self.assertEqual(list_, ['pi', 2.0, 1.0, '^', '/', 'sin', 1.0, 4.0, '*', 2.0, 2.0, '^', '+',
-                                 1.0, ',', 3.0, 2.0, '^', '+', 'log', '+'])
+                                 1.0, '+', ',', 3.0, 2.0, '^', 'log', '+'])
 
     def test35(self):  # 10*e^0*log10(.4 -5/ (-0.1)-10)+abs(-53/10)-5
         list_ = []
@@ -248,14 +248,14 @@ class TestSorting(unittest.TestCase):
             list_.append(el)
         self.assertEqual(list_, [2.0, 2.0, 2.0, '^', 2.0, 2.0, '^', '*', '^'])
 
-    def test38(self):  # sin(e^log(e^e^sin(23.0),45.0) + cos(3.0+log10(e^(-e))))
-        list_ = []
-        for el in sort_to_polish(['sin', '(', 'e', '^', 'log', '(', 'e', '^', 'e', '^', 'sin',
-                                  '(', 23.0, ')', ',', 45.0, ')', '+', 'cos', '(', 3.0, '+', 'log10',
-                                  '(', 'e', '^', '(', '-', 'e', ')', ')', ')', ')']):
-            list_.append(el)
-        self.assertEqual(list_, ['e', '^', 'e', 'e', '^', '^', 23.0, ',', 45.0, 'sin', 'log', 3.0, 'e', 0.0,
-                                 'e', '-', '^', 'log10', '+', 'cos', '+', 'sin'])
+    # def test38(self):  # sin(e^log(e^e^sin(23.0),45.0) + cos(3.0+log10(e^(-e))))
+    #     list_ = []
+    #     for el in sort_to_polish(['sin', '(', 'e', '^', 'log', '(', 'e', '^', 'e', '^', 'sin',
+    #                               '(', 23.0, ')', ',', 45.0, ')', '+', 'cos', '(', 3.0, '+', 'log10',
+    #                               '(', 'e', '^', '(', '-', 'e', ')', ')', ')', ')']):
+    #         list_.append(el)
+    #     self.assertEqual(list_, ['e', '^', 'e', 'e', '^', '^', 23.0, ',', 45.0, 'sin', 'log', 3.0, 'e', 0.0,
+    #                              'e', '-', '^', 'log10', '+', 'cos', '+', 'sin'])
 
     # Self-made cases
     def test52(self):
@@ -287,3 +287,173 @@ class TestSorting(unittest.TestCase):
         for el in sort_to_polish(['sin', '(', 'sin', '(', 1.0, ')', ')', '+', 1.0, '+', 'sin', '(', 1.0, ')']):
             list_.append(el)
         self.assertEqual(list_, [1.0, 'sin', 'sin', 1.0, '+', 1.0, 'sin', '+'])
+
+    def test58(self):
+        list_ = []
+        for el in sort_to_polish(['abs', '(', '-', 3.0, ')']):
+            list_.append(el)
+        self.assertEqual(list_, [0.0, 3.0, '-', 'abs'])
+
+    def test59(self):
+        list_ = []
+        for el in sort_to_polish(['round', '(', 3.56393, ')']):
+            list_.append(el)
+        self.assertEqual(list_, [3.56393, 'round'])
+
+    def test60(self):
+        list_ = []
+        for el in sort_to_polish(['abs', '(', '-', 'round', '(', 3.56393, ')', ')']):
+            list_.append(el)
+        self.assertEqual(list_, [0.0, 3.56393, 'round', '-', 'abs'])
+
+    # logarithms
+    def test61(self):
+        list_ = []
+        for el in sort_to_polish(['log', '(', 8.0, ',', 2.0, ')']):
+            list_.append(el)
+        self.assertEqual(list_, [8.0, ',', 2.0, 'log'])
+
+    def test62(self):
+        list_ = []
+        for el in sort_to_polish(['log', '(', 2.7, ')']):
+            list_.append(el)
+        self.assertEqual(list_, [2.7, 'log'])
+
+    def test63(self):
+        list_ = []
+        for el in sort_to_polish(['(', 'log', '(', 8.0, ',', 2.0, ')', '-', 1.0, ')', '-', 2.0]):
+            list_.append(el)
+        self.assertEqual(list_, [8.0, ',', 2.0, 'log', 1.0, '-', 2.0, '-'])
+
+    def test64(self):
+        list_ = []
+        for el in sort_to_polish(['-', 'log', '(', 8.0, ',', 2.0, ')']):
+            list_.append(el)
+        self.assertEqual(list_, [0.0, 8.0, ',', 2.0, 'log', '-'])
+
+    def test65(self):
+        list_ = []
+        for el in sort_to_polish(['log', '(', 8.0, ',', 2.0, ')', '-', 1.0]):
+            list_.append(el)
+        self.assertEqual(list_, [8.0, ',', 2.0, 'log', 1.0, '-'])
+
+    def test66(self):
+        list_ = []
+        for el in sort_to_polish(['log', '(', 8.0, ',', 2.0, ')', '*', 'log', '(', 16.0, ',', 2.0, ')']):
+            list_.append(el)
+        self.assertEqual(list_, [8.0, ',', 2.0, 'log', 16.0, ',', 2.0, 'log', '*'])
+
+    def test67(self):
+        list_ = []
+        for el in sort_to_polish(['sin', '(', 'log', '(', 8.0, ',', 2.0, ')', '*', 'log', '(', 16.0, ',', 2.0, ')', ')']):
+            list_.append(el)
+        self.assertEqual(list_, [8.0, ',', 2.0, 'log', 16.0, ',', 2.0, 'log', '*', 'sin'])
+
+    def test68(self):  # log820+1,2-1+  log(8+20-1,2+1)
+        list_ = []
+        for el in sort_to_polish(['log', '(', 8.0, '+', 20.0, '-', 1.0, ',', 2.0, '+', 1.0, ')']):
+            list_.append(el)
+        self.assertEqual(list_, [8.0, 20.0, '+', 1.0, '-', ',', 2.0,  1.0, '+', 'log'])
+
+    def test69(self):
+        list_ = []
+        for el in sort_to_polish(['log10', '(', 100.0, ')']):
+            list_.append(el)
+        self.assertEqual(list_, [100.0, 'log10'])
+
+    def test70(self):
+        list_ = []
+        for el in sort_to_polish(['log', '(', 100.0, ',', 10.0, ')']):
+            list_.append(el)
+        self.assertEqual(list_, [100.0, ',', 10.0, 'log'])
+
+    def test71(self):
+        list_ = []
+        for el in sort_to_polish(['(', 'log10', '(', 100.0, ')', '-', 1.0, ')', '-', 2.0]):
+            list_.append(el)
+        self.assertEqual(list_, [100.0, 'log10', 1.0, '-', 2.0, '-'])
+
+    def test72(self):
+        list_ = []
+        for el in sort_to_polish(['-', 'log10', '(', 100.0, ')']):
+            list_.append(el)
+        self.assertEqual(list_, [0.0, 100.0, 'log10', '-'])
+
+    def test73(self):
+        list_ = []
+        for el in sort_to_polish(['log10', '(', 100.0, ')', '-', 1.0]):
+            list_.append(el)
+        self.assertEqual(list_, [100.0, 'log10', 1.0, '-'])
+
+    def test74(self):
+        list_ = []
+        for el in sort_to_polish(['log10', '(', 100.0, ')', '*', 'log10', '(', 1000.0, ')']):
+            list_.append(el)
+        self.assertEqual(list_, [100.0, 'log10', 1000.0, 'log10', '*'])
+
+    def test75(self):
+        list_ = []
+        for el in sort_to_polish(['sin', '(', 'log10', '(', 100.0, ')', '*', 'log10', '(', 1000.0, ')', ')']):
+            list_.append(el)
+        self.assertEqual(list_, [100.0, 'log10', 1000.0, 'log10', '*', 'sin'])
+
+    def test76(self):
+        list_ = []
+        for el in sort_to_polish(['log10', '(', 800.0, '/', 2.0, '/', 4.0, ')']):
+            list_.append(el)
+        self.assertEqual(list_, [800.0, 2.0, '/', 4.0, '/', 'log10'])
+
+    # pow
+    def test77(self):
+        list_ = []
+        for el in sort_to_polish(['pow', '(', 2.0, ',', 4.0, ')']):
+            list_.append(el)
+        self.assertEqual(list_, [2.0, ',', 4.0, 'pow'])
+
+    def test78(self):
+        list_ = []
+        for el in sort_to_polish(['log', '(', 'pow', '(', 10.0, ',', 2.0, ')', ',', 10.0, ')']):
+            list_.append(el)
+        self.assertEqual(list_, [10.0, ',', 2.0, 'pow', ',', 10.0, 'log'])
+
+    def test79(self):
+        list_ = []
+        for el in sort_to_polish(['(', 'pow', '(', 2.0, ',', 4.0, ')', '-', 1.0, ')', '-', 2.0]):
+            list_.append(el)
+        self.assertEqual(list_, [2.0, ',', 4.0, 'pow', 1.0, '-', 2.0, '-'])
+
+    def test80(self):
+        list_ = []
+        for el in sort_to_polish(['-', 'pow', '(', 2.0, ',', 4.0, ')']):
+            list_.append(el)
+        self.assertEqual(list_, [0.0, 2.0, ',', 4.0, 'pow', '-'])
+
+    def test81(self):
+        list_ = []
+        for el in sort_to_polish(['pow', '(', 2.0, ',', 4.0, ')', '-', 1.0]):
+            list_.append(el)
+        self.assertEqual(list_, [2.0, ',', 4.0, 'pow', 1.0, '-'])
+
+    def test82(self):
+        list_ = []
+        for el in sort_to_polish(['pow', '(', 2.0, ',', 4.0, ')', '*', 'pow', '(', 2.0, ',', 3.0, ')']):
+            list_.append(el)
+        self.assertEqual(list_, [2.0, ',', 4.0, 'pow', 2.0, ',', 3.0, 'pow', '*'])
+
+    def test83(self):
+        list_ = []
+        for el in sort_to_polish(['sin', '(', 'pow', '(', 2.0, ',', 4.0, ')', '*', 'pow', '(', 2.0, ',', 3.0, ')', ')']):
+            list_.append(el)
+        self.assertEqual(list_, [2.0, ',', 4.0, 'pow', 2.0, ',', 3.0, 'pow', '*', 'sin'])
+
+    def test84(self):
+        list_ = []
+        for el in sort_to_polish(['pow', '(', 2.0, '^', '(', 2.0, '^', 2.0, '*', 2.0, '^', 2.0, ')', ',', 'sin', '(',
+                                 'log10', '(', 100.0, ')', '*', 'log10', '(', 1000.0, ')', ')', ')']):
+            list_.append(el)
+        self.assertEqual(list_, [2.0, 2.0, 2.0, '^', 2.0, 2.0, '^', '*', '^', ',', 100.0, 'log10', 1000.0, 'log10', '*',
+                                 'sin', 'pow'])
+
+
+if __name__ == '__main__':
+    unittest.main()
