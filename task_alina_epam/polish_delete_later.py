dacf2299bc6c13476d7357da0decfcf128d5a27a
dlaevskiy <dlaevskiy@kyriba.com>
2019-05-30 14:56:30 +0300

    fix 30 05 - 4

--- task_alina_epam/polish_delete_later.py
+++ task_alina_epam/polish_delete_later.py
@@ -18,15 +18,21 @@ class TestSorting(unittest.TestCase):
     #         list_.append(el)
     #     self.assertEqual(list_, [1.0, 0.0, 13.0, '-', '*'])
 
-    # TODO: process log correctly (if we put here first argument of log in (), everything will be done correctly)
-    # TODO: problem, when unary operations go after binary without parentheses as e^-e
-    def test38(self):  # sin(e^log(e^e^sin(23.0),45.0) + cos(3.0+log10(e^-e)))
+    # # TODO: process log correctly (if we put here first argument of log in (), everything will be done correctly)
+    # # TODO: problem, when unary operations go after binary without parentheses as e^-e
+    # def test38(self):  # sin(e^log(e^e^sin(23.0),45.0))
+    #     list_ = []
+    #     for el in sort_to_polish(['sin', '(', 'e', '^', 'log', '(', 'e', '^', 'e', '^', 'sin',
+    #                               '(', 23.0, ')', ',', 45.0, ')', ')']):
+    #         list_.append(el)
+    #     self.assertEqual(list_, ['e', 'e', 'e', 23.0, 'sin', '^', '^',  ',',
+    #                              45.0, 'log', '^', 'sin'])
+
+        # self.assertEqual(list_, ['e', 'e', 'e', 23.0, 'sin', '^', '^', ',', 45.0, 'log', '^', 3.0, 'e', 0.0,
+        #                          'e', '-', '^', 'log10', '+', 'cos', '+', 'sin'])
+
+    def test39(self):  # sin(e^cos(e^e))
         list_ = []
-        for el in sort_to_polish(['sin', '(', 'e', '^', 'log', '(', 'e', '^', 'e', '^', 'sin',
-                                  '(', 23.0, ')', ',', 45.0, ')', '+', 'cos', '(', 3.0, '+', 'log10',
-                                  '(', 'e', '^', '-', 'e', ')', ')', ')']):
+        for el in sort_to_polish(['e', '^', 'cos', '(', 'e', '^', 'e', ')']):
             list_.append(el)
-        self.assertEqual(list_, ['e', 'e', 'e', 23.0, 'sin', '^', '^',  ',',
-                                 45.0, 'log', '^',
-                                 3.0, 'e', 0.0, 'e', '-', '^', 'log10', '+', 'cos', '+',
-                                 'sin'])
+        self.assertEqual(list_, ['e', 'e', 'e', '^', 'cos', '^'])
