3d7de1e024edfdad7ec512a93e93f181db78c960
dlaevskiy <dlaevskiy@kyriba.com>
2020-03-10 11:09:28 +0300

    fix

--- coursera_web_python/api_currency_converter/converter_sample/currency.py
+++ coursera_web_python/api_currency_converter/converter_sample/currency.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 
 # import requests
 
-QUANTIZE = Decimal('1.0000')
+CROSS_CUR = 'RUR'
 
 
 def _parse_xml(soup, currency):
@@ -13,7 +13,7 @@ def _parse_xml(soup, currency):
         nominal = char_code.find_next_sibling('Nominal').text
         one_rur_cost = Decimal(nominal) / Decimal(cur_exchange)
     except AttributeError:
-        print('Currrency "{}" from was not found during bank request!'.format(currency))
+        print('Currency "{}" was not found during bank request!'.format(currency))
         char_code = cur_exchange = nominal = one_rur_cost = ''
     return char_code, cur_exchange, nominal, one_rur_cost
 
@@ -25,13 +25,14 @@ def convert(amount, cur_from, cur_to, date, requests):
     char_code_from, cur_exchange_from, nominal_from, one_rur_cost_from = _parse_xml(soup, cur_from)
     char_code_to, cur_exchange_to, nominal_to, one_rur_cost_to = _parse_xml(soup, cur_to)
 
-    if cur_from != 'RUR' and cur_to != 'RUR':
-        result = (one_rur_cost_to / one_rur_cost_from * Decimal(amount)).quantize(QUANTIZE)
-    elif cur_from == 'RUR' and cur_to != 'RUR':
-        result = (one_rur_cost_to * Decimal(amount)).quantize(QUANTIZE)
-    elif cur_from != 'RUR' and cur_to == 'RUR':
-        result = (Decimal(amount) / one_rur_cost_from).quantize(QUANTIZE)
-    else:
-        result = Decimal(amount).quantize(Decimal(QUANTIZE))
+    amount = Decimal(amount)
 
-    return result
+    result = amount
+    if cur_from != CROSS_CUR and cur_to != CROSS_CUR:
+        result = (one_rur_cost_to / one_rur_cost_from) * amount
+    elif cur_from == CROSS_CUR and cur_to != CROSS_CUR:
+        result = (one_rur_cost_to * amount)
+    elif cur_from != CROSS_CUR and cur_to == CROSS_CUR:
+        result = amount / one_rur_cost_from
+
+    return result.quantize(Decimal('1.0000'))
