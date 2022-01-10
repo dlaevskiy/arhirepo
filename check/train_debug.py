e0230750ef0c262ea45848c9d2d889fa18f67eff
dla <31095616+dlaevskiy@users.noreply.github.com>
2022-01-10 13:49:13 +0100

    Update train_debug.py

--- check/train_debug.py
+++ check/train_debug.py
@@ -1,19 +1,23 @@
-def name_generator(nameNumber, *values):
+def get_name_cases(nameNumber, *values):
     genNames = ['' for i in range(nameNumber)]
     for element in values:
         fieldName = element.split('#')[0]
         values = element.split('#')[1].split(';')
         diffLen = len(genNames) - len(values)
         L = [values[-1]] * diffLen
-        newvalues = values + L
+        newValues = values + L
         counter = 0
-        for value in newvalues:
+        for value in newValues:
             genNames[counter] += fieldName + value + ' | '
             counter += 1
     return genNames
 
 
-for name in name_generator(12, 'ACCOUNT_NATURE#=2;<>2',
-                               'DEBIT_MODE#=1;<>1;=2;<>2;=3;<>3;=4;<>4;=5;<>5;<>1,2,3,4,5;=1,2,3,4,5',
-                               'TYPE#=1;<>1'):
+values = (
+    'Set account nature#=2;<>2',
+    'Set debit mode#=1;<>1;=2;<>2;=3;<>3;=4;<>4;=5;<>5;<>1,2,3,4,5;=1,2,3,4,5',
+    'Set account type#=1;<>1'
+)
+
+for name in get_name_cases(12, *values):
     print(name.rstrip(' | '))
